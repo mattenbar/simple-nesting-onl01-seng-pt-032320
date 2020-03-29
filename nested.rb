53af8cdeaf218ce67fc42da50daa1970c9d0bec3
matthew enbar <matthew.enbar@gmail.com>
2020-03-29 17:52:26 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -74,14 +74,14 @@ def adding_matz
         },
         :alan_kay => {
           :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
+          :languages => ["GUI"]
         },
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
         },
         :yukihiro_matsumoto =>{
-          
+
         }
      }
 
