dcf06ff7468c2d924116a0bbf0e5a533ea7435cd
matthew enbar <matthew.enbar@gmail.com>
2020-03-29 18:08:18 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -80,10 +80,11 @@ def adding_matz
           :known_for => "Unix",
           :languages => ["C"]
         }
-        #:yukihiro_matsumoto =>{
-        #  :known_for => "Ruby",
-        #  :languages => ["LISP", "C"]
+        :yukihiro_matsumoto =>{
+          :known_for => "Ruby",
+          :languages => ["LISP", "C"]
         }
+    }
 
 
   programmer_hash[:alan_kay][:known_for] = "GUI"
